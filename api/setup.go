2929a728630ce4c6f35c3e47558610a080286173
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-07-22 10:26:37 +0100

    Update routes to use gorilla/mux

--- api/setup.go
+++ api/setup.go
@@ -3,9 +3,13 @@ package api
 import (
 	routes "go-lang-web-service/api/routes"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 //Hooks up all the api's endpoints
 func HookRoutes() {
-	http.HandleFunc("/routes", routes.GetRoutes)
+	router := mux.NewRouter()
+	router.HandleFunc("/routes", routes.Controller.GetRoutes).Methods("GET")
+	http.Handle("/", router)
 }
